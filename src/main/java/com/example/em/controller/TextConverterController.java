66e1d65ba0425d755b5623f439032e61665830f9
h_maeda <h_maeda@192.168.11.51>
2024-10-15 17:58:19 +0900

    文字コードの判定よくわからん

--- src/main/java/com/example/em/controller/TextConverterController.java
+++ src/main/java/com/example/em/controller/TextConverterController.java
@@ -1,5 +1,11 @@
 package com.example.em.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Controller
 @RequestMapping("/TextConverter")
 public class TextConverterController {
@@ -23,8 +30,22 @@ public class TextConverterController {
 			@RequestParam("options") String encoding,
 			RedirectAttributes redirectAttributes) {
 
-		
+		try {
+			InputStream stream = file.getInputStream();
+			Reader reader = new InputStreamReader(stream, "UTF-8");
+			BufferedReader buf = new BufferedReader(reader);
 
-		return "download";
+			String str;
+
+			while ((str = buf.readLine()) != null) {
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "redirect:/TextConverter/upload";
 	}
+
 }
